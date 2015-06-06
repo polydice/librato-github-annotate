7c57bacdacb3af8413172924c274a67420366059
frozenfung <frozenfung@gmail.com>
2015-06-06 23:56:15 +0800

    Take off config variable

--- config/rainbow.rb
+++ config/rainbow.rb
@@ -1,4 +1,4 @@
-worker_processes ENV["PROCESS_COUNT"] # amount of rainbow workers to spin up
+worker_processes 2 # amount of rainbow workers to spin up
 timeout 15         # restarts workers that hang for 30 seconds
 preload_app true   # reduce mem footprint
 
